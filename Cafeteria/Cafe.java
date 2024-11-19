2dd0343e8916d80e34af3ad94d94b64d8af0d684
Daniel Diaz <167503159+DanielDiaz45@users.noreply.github.com>
2024-11-19 20:40:02 -0300

    Add files via upload

---
+++ Cafeteria/Cafe.java
@@ -0,0 +1,22 @@
+package Cafeteria;
+
+public abstract class Cafe {
+    protected double precioBase;
+	
+	public  Cafe() {
+	}
+	public Cafe(double precioBase) {
+		super();
+		this.precioBase = precioBase;
+	}
+	public double getPrecioBase() {
+		return precioBase;
+	}
+	public void setPrecioBase(double precioBase) {
+		this.precioBase = precioBase;
+	}
+	
+	public abstract double calcularPrecio(); 
+		
+    
+}
